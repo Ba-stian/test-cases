dab5a13c5b9f5d5d74e259df3725a26e0aa2d1ac
Ba_stian <k.batakovich@gmail.com>
2019-05-13 11:38:18 +0300

    Create geo-script

--- test-case-2/js/main.js
+++ test-case-2/js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function () {
+	(function Geo() {
+		$.ajax('https://ipapi.co/json')
+			.then(function success(data) {
+				country = data.country;
+				city = data.city;
+				if (country === 'RU') {
+					if (city === 'St Petersburg') {
+						$('#replace').replaceWith('Санкт-Петербурге')
+					} else {
+						$('#replace').replaceWith(data.city)
+					}
+				} else {
+					$('#replace').replaceWith('Странах СНГ')
+				}
+			})
+	}());
 
+});
